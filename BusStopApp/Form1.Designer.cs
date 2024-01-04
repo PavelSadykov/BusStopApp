55727940cac6c07b82e86d57dbc017030117f0d6
PavelSadykov <2439747@gmail.com>
2024-01-04 15:09:25 +0500

    Добавьте файлы проекта.

---
+++ BusStopApp/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace BusStopApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArrive = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnArrive
+            // 
+            this.btnArrive.Location = new System.Drawing.Point(122, 179);
+            this.btnArrive.Name = "btnArrive";
+            this.btnArrive.Size = new System.Drawing.Size(75, 23);
+            this.btnArrive.TabIndex = 0;
+            this.btnArrive.Text = "Arrive";
+            this.btnArrive.UseVisualStyleBackColor = true;
+            this.btnArrive.Click += new System.EventHandler(this.btnArrive_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(225, 102);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(532, 206);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(122, 102);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnArrive);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArrive;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ BusStopApp/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BusStopApp
+{
+    public partial class Form1 : Form
+    {
+        private BusStop busStop;
+        private List<Bus> buses = new List<Bus>();
+        int passengersOnBoard;
+        public Form1()
+        {
+            InitializeComponent();
+            busStop = new BusStop(40, 20); // начальное количество пассажиров и вместимость автобуса
+
+            // Создаем  автобусы с разными номерами.
+            for (int i = 1; i <= 3; i++) 
+            {
+                buses.Add(new Bus(busStop, i));
+            }
+        }
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            Thread stopThread = new Thread(() =>
+            {
+                busStop.Run((message) =>
+                {
+                    UpdateTextBox(message);
+                });
+            });
+            stopThread.IsBackground = true;
+            stopThread.Start();
+
+            Thread busThread = new Thread(() =>
+            {
+                Random random = new Random();
+                while (true)
+                {
+                    passengersOnBoard = random.Next(0, 21); // Случаное количество пассажиров в автобусе
+                    Thread.Sleep(random.Next(3000, 10000)); // Случайное время интервала прибытия автобусов
+                    foreach (var bus in buses)
+                    {
+                        bus.Arrive(passengersOnBoard);
+                    }
+                }
+            });
+            busThread.IsBackground = true;
+            busThread.Start();
+
+            MessageBox.Show("Симуляция запущена.");
+        }
+        // Метод для безопасного обновления текстового поля на форме из другого потока
+        private void UpdateTextBox(string message)
+        {
+            if (textBox1.InvokeRequired)
+            {
+                textBox1.Invoke(new Action<string>(UpdateTextBox), new object[] { message });
+            }
+            else
+            {
+                textBox1.AppendText(message + Environment.NewLine);
+            }
+        }
+
+        private void btnArrive_Click_1(object sender, EventArgs e)
+        {
+
+            foreach (var bus in buses)
+            {
+                bus.Arrive(passengersOnBoard);
+            }
+        }
+    }
+
+    public class BusStop
+    {   public object lockObj = new object();
+        private Dictionary<int,int> passengersByBusNumber = new Dictionary<int,int>();  
+        private int peopleAtStop; // количество людей на остановке
+        private int busCapacity; // вместимость автобуса
+       
+
+        // Событие для сигнализации о прибытии автобуса
+        private AutoResetEvent busArrival = new AutoResetEvent(false);
+
+        public BusStop(int initialPeople, int capacity)
+        {
+            peopleAtStop = initialPeople;
+            busCapacity = capacity;
+        }
+
+        public void Run(Action<string> messageCallback)
+        {
+            Random random = new Random();
+
+            while (true)
+            {
+                int waitingPassengers = random.Next(1, 11); // случайное число людей на остановке
+                messageCallback?.Invoke($"На остановку пришло {waitingPassengers} пассажиров.");
+
+                lock (lockObj)
+                {
+                    for (int i = 0; i < waitingPassengers; i++)
+                    {
+                        int passengerBusNumber = random.Next(1, 4); // Случайный номер автобуса
+                        if (passengersByBusNumber.ContainsKey(passengerBusNumber))
+                        {
+                            passengersByBusNumber[passengerBusNumber]++;
+                        }
+                        else
+                        {
+                            passengersByBusNumber.Add(passengerBusNumber, 1);
+                        }
+                        messageCallback?.Invoke($"На остановку пришло:{passengersByBusNumber[passengerBusNumber]} пассажиров, ожидающих автобус №{passengerBusNumber}. ");
+                    }
+
+
+
+                      peopleAtStop += waitingPassengers; // добавляем новых пассажиров на остановку
+                }
+
+                messageCallback?.Invoke($"Текущее количество пассажиров на остановке: {peopleAtStop}");
+                // Имитация времени между прибытием автобусов
+                Thread.Sleep(random.Next(1000, 5000));
+                // Ожидание прибытия автобуса
+                busArrival.WaitOne();
+
+                lock (lockObj)
+                {
+                    int passengersToBoard = Math.Min(peopleAtStop, busCapacity); // число пассажиров, которые могут уехать на автобусе
+                    peopleAtStop -= passengersToBoard; // высаживаем пассажиров из автобуса
+                    messageCallback?.Invoke($"Автобус уехал, пассажиров на остановке осталось: {peopleAtStop}");
+                }
+
+                
+            }
+        }
+
+        public int GetPassengersForBus(int busNumber)
+        {
+            lock (lockObj)
+            {
+                return passengersByBusNumber.ContainsKey(busNumber) ? passengersByBusNumber[busNumber] : 0;
+            }
+        }
+
+        public int GetPeopleAtStop()
+        {
+            return peopleAtStop;
+        }
+        public void UpdatePeopleAtStop(int passengersToBoard)
+        {
+            peopleAtStop -= passengersToBoard;
+        }
+
+        public void BusArrived()
+        {
+            busArrival.Set(); // сигнал для автобуса о прибытии
+        }
+    }
+
+    public class Bus
+    {
+        private int busNumber;
+        private BusStop busStop;
+
+        public Bus(BusStop stop, int number)
+        {
+            busStop = stop;
+            busNumber = number;
+        }
+
+        public void Arrive(int passengersOnBoard)
+        {
+            MessageBox.Show($"Автобус №{busNumber} приехал на остановку с {passengersOnBoard} пассажирами на борту.");
+
+            int passengersToBoard = Math.Min(passengersOnBoard, busStop.GetPeopleAtStop());
+
+            lock (busStop.lockObj) 
+            {
+                busStop.UpdatePeopleAtStop(passengersToBoard);
+                Thread.Sleep(2000);
+
+                MessageBox.Show($"Автобус №{busNumber} взял {passengersToBoard} пассажиров.");
+                Thread.Sleep(3000);
+                if (passengersToBoard < passengersOnBoard)
+                {
+                    Thread.Sleep(2000);
+                    MessageBox.Show($"Оставшиеся пассажиры уедут на следующем автобусе.");
+                }
+            }
+        }
+    }
+}
